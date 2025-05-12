45b721a140d0f9f56b898cf24dde181ea6a8c192
ntthanh2603 <tuanthanh2kk4@gmail.com>
2025-05-12 14:39:54 +0700

    refacter

--- cmd/server/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/ntthanh2603/golang.git/internal/routers"
-
-func main() {
-	r := routers.NewRouter()
-
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
--- tests/basic/main.go
+++
@@ -1,5 +0,0 @@
-package basic
-
-func AddOne(num int) int {
-	return num + 1
-}
