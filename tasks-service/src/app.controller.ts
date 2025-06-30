7eabb68b0143615af5a9e00f0d1e51f2e7746c50
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-06-30 23:46:17 +0700

    fix: add Payload decorator to get_tasks method in AppController

--- tasks-service/src/app.controller.ts
+++ tasks-service/src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @MessagePattern('get_tasks')
-  get(data: string): string {
+  get(@Payload() data: string): string {
     return data;
   }
 }
